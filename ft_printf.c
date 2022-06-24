701d25811cf034437294b96a7e88d356f3c669be
Bruno Pinheiro <bda-silv@student.42.rio>
2022-06-24 06:19:43 -0300

    f_arg: c, s, d

--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,74 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 14:27:17 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/23 11:50:00 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/24 06:15:14 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* TODO:
+ * - read params
+ * - sort params
+ *   printf("", a, b, c)
+ *   no ponteiro, unsigned long
+ *	ft_putstr_fd(va_arg(arg, char *), 1);
+ * */
+
 #include "ft_printf.h"
+
+static int
+	ft_parse(char c, va_list ap)
+{
+	int		len;
+
+	len = 0;
+	if (c == 'c')
+		len = f_arg_c(ap);
+	if (c == 's')
+		len = f_arg_s(ap);
+	if (c == 'd')
+		len = f_arg_d(ap);
+	return (len);
+}
 
 int
 	ft_printf(const char *str, ...)
 {
-	va_list	arg;
+	va_list	ap;
+	int		i;
+	int		r;
 
-	va_start (arg, str);
-	ft_putstr_fd((char *)str, 1);
-	ft_putstr_fd(va_arg(arg, char *), 1);
-	ft_putstr_fd(va_arg(arg, char *), 1);
-	va_end (arg);
-	return (0);
+	i = 0;
+	r = 0;
+	va_start (ap, str);
+	if (!str)
+		return (i);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%' && *ft_strchr("cspdiuxX%", str[i + 1]))
+		{
+			i++;
+			r += ft_parse(str[i], ap);
+		}
+		else
+		{
+			r++;
+			ft_putchar_fd(str[i], 1);
+		}
+		i++;
+	}
+	va_end (ap);
+	/*
+	puts(" ");
+	ft_putnbr_fd(r, 1);
+	puts("\n");
+	*/
+	return (r);
 }
 
 int
 	main(void)
 {
-	ft_printf("bla\n", "ble\n", "blu\n");
+	ft_printf("blew %s%c %d\n", "blo", '!', -2035065302);
+	printf("blew %s%c %d\n", "blo", '!', -2035065302);
 	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bda-silv <bda-silv@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 14:11:48 by bda-silv          #+#    #+#             */
-/*   Updated: 2022/06/23 13:26:39 by bda-silv         ###   ########.fr       */
+/*   Updated: 2022/06/24 06:08:45 by bda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,19 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include "libft/libft.h"
+# include <stdio.h> // TODO: Remove this header before Evaluation
 
 int	ft_printf(const char *str, ...);
-
+int	f_arg_c(va_list ap);
+int	f_arg_s(va_list ap);
+int	f_arg_d(va_list ap);
+/*
+int	f_arg_p(va_list ap);
+int	f_arg_d(va_list ap);
+int	f_arg_i(va_list ap);
+int	f_arg_u(va_list ap);
+int	f_arg_x(va_list ap);
+int	f_arg_X(va_list ap);
+int	f_arg_pc(va_list ap);
+*/
 #endif
